346db420719d1938464dd728a99967d2ec954d04
Sirawit Moonrinta <sirawit_m@cmu.ac.th>
2017-03-29 08:51:36 +0700

    speech innit

--- WebControl/src/app/speech/speech.component.ts
+++ WebControl/src/app/speech/speech.component.ts
@@ -1,14 +1,53 @@
-import { Component, OnInit} from '@angular/core';
-import { SpeechService } from '../shared/speech.service';
+import { Component, OnInit, OnDestroy} from '@angular/core';
+import { SpeechRecognitionService } from './speech.service';
 
 @Component({
   selector: 'app-speech',
   templateUrl: './speech.component.html',
   styleUrls: ['./speech.component.scss']
 })
-export class SpeechComponent implements OnInit {
+export class SpeechComponent implements OnInit, OnDestroy {
 
-    constructor() { }
+    showSearchButton: boolean;
+    speechData: string;
 
-    ngOnInit() { }
+    constructor(private speechRecognitionService: SpeechRecognitionService) {
+        this.showSearchButton = true;
+        this.speechData = "";
+    }
+
+    ngOnInit() {
+        console.log("hello")
+    }
+
+    ngOnDestroy() {
+        this.speechRecognitionService.DestroySpeechObject();
+    }
+
+    activateSpeechSearchMovie(): void {
+        this.showSearchButton = false;
+
+        this.speechRecognitionService.record()
+            .subscribe(
+            //listener
+            (value) => {
+                this.speechData = value;
+                console.log(value);
+            },
+            //errror
+            (err) => {
+                console.log(err);
+                if (err.error == "no-speech") {
+                    console.log("--restatring service--");
+                    this.activateSpeechSearchMovie();
+                }
+            },
+            //completion
+            () => {
+                this.showSearchButton = true;
+                console.log("--complete--");
+                this.activateSpeechSearchMovie();
+            });
+    }
+
 }
