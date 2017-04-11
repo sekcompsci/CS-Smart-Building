dde108bd8b3fe2ec8ad267e1f385e800a198f972
Sirawit Moonrinta <sirawit_m@cmu.ac.th>
2017-04-11 13:03:11 +0700

    back to complete

--- WebControl/src/app/speech/speech.component.ts
+++ WebControl/src/app/speech/speech.component.ts
@@ -1,53 +1,14 @@
-import { Component, OnInit, OnDestroy} from '@angular/core';
-import { SpeechRecognitionService } from './speech.service';
+import { Component, OnInit} from '@angular/core';
+import { SpeechService } from '../shared/speech.service';
 
 @Component({
   selector: 'app-speech',
   templateUrl: './speech.component.html',
   styleUrls: ['./speech.component.scss']
 })
-export class SpeechComponent implements OnInit, OnDestroy {
+export class SpeechComponent implements OnInit {
 
-    showSearchButton: boolean;
-    speechData: string;
+    constructor() { }
 
-    constructor(private speechRecognitionService: SpeechRecognitionService) {
-        this.showSearchButton = true;
-        this.speechData = "";
-    }
-
-    ngOnInit() {
-        console.log("hello")
-    }
-
-    ngOnDestroy() {
-        this.speechRecognitionService.DestroySpeechObject();
-    }
-
-    activateSpeechSearchMovie(): void {
-        this.showSearchButton = false;
-
-        this.speechRecognitionService.record()
-            .subscribe(
-            //listener
-            (value) => {
-                this.speechData = value;
-                console.log(value);
-            },
-            //errror
-            (err) => {
-                console.log(err);
-                if (err.error == "no-speech") {
-                    console.log("--restatring service--");
-                    this.activateSpeechSearchMovie();
-                }
-            },
-            //completion
-            () => {
-                this.showSearchButton = true;
-                console.log("--complete--");
-                this.activateSpeechSearchMovie();
-            });
-    }
-
+    ngOnInit() { }
 }
--- WebControl/src/typings.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'typeless-package';
