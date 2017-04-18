79b3d046e0745db15da1412f8ebc47f89e23e8e5
Sirawit Moonrinta <sirawit_m@cmu.ac.th>
2017-04-18 13:55:43 +0700

    Success: WebkitSpeechRecognition and Impliment Backend

--- WebControl/src/app/speech/speech.component.ts
+++ WebControl/src/app/speech/speech.component.ts
@@ -1,17 +1,51 @@
-import { Component } from '@angular/core';
-// import { Speech } from '@google-cloud/speech';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SpeechService } from '../shared/speech.service';
 
 @Component({
   selector: 'app-speech',
   templateUrl: './speech.component.html',
   styleUrls: ['./speech.component.scss']
 })
-export class SpeechComponent {
+export class SpeechComponent implements OnInit, OnDestroy {
+  showSearchButton: boolean;
+  speechData: string;
 
-    // constructor() {
-    //   var speech = Speech({
-    //     projectId: 'grape-spaceship-123',
-    //     keyFilename: '/path/to/keyfile.json'
-    //   });
-    // }
+  constructor(private speechRecognitionService: SpeechService) {
+    this.showSearchButton = true;
+    this.speechData = "";
+  }
+
+  ngOnInit() {
+    console.log("hello")
+  }
+
+  ngOnDestroy() {
+    this.speechRecognitionService.DestroySpeechObject();
+  }
+
+  activateSpeechSearchMovie(): void {
+    this.showSearchButton = false;
+
+    this.speechRecognitionService.record()
+      .subscribe(
+      //listener
+      (value) => {
+        this.speechData = value;
+        console.log(value);
+      },
+      //errror
+      (err) => {
+        console.log(err);
+        if (err.error == "no-speech") {
+          console.log("--restatring service--");
+          this.activateSpeechSearchMovie();
+        }
+      },
+      //completion
+      () => {
+        this.showSearchButton = true;
+        console.log("--complete--");
+        this.activateSpeechSearchMovie();
+      });
+  }
 }
